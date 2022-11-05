91fb8bfba65ca96522456813488335b805bd8582
Роман <pangujailbreake58@gmail.com>
2022-11-05 20:09:36 +0300

    второй

--- src/Author.java
+++ src/Author.java
@@ -1,4 +1,18 @@
+import java.util.Objects;
+
 public class Author {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Author)) return false;
+        Author author = (Author) o;
+        return getNameAuthor().equals(author.getNameAuthor()) && getSurnameAuthor().equals(author.getSurnameAuthor());
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNameAuthor(), getSurnameAuthor());
+    }
+
     private String nameAuthor;
     private String surnameAuthor;
 
@@ -14,5 +28,15 @@ public class Author {
     public String getSurnameAuthor() {
         return this.surnameAuthor;
     }
+public String toString (){
+        return nameAuthor+"  "+ surnameAuthor;
+}
 
+    public boolean equals (Author nameAuthor){
+        if (this.getNameAuthor() == nameAuthor.getNameAuthor())
+        {
+            return true;
+        } else return  false;
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,15 @@ public class Main {
 
         //передаем объект authorOne в качестве параметра конструктора
         Book bookOne = new Book("Roman s kamnem", authorOne, 1985);
-        Book bookTwo = new Book("Dunno on the Moon", authorTwo, 1965);
+        Book bookTwo = new Book("Dunno on the Moon", authorTwo, 1985);
 
         System.out.println(bookOne.getNameBook() + "  " + bookOne.getYearPublication() + " " + authorOne.getNameAuthor());
-        bookOne.setYearPublication(1966);
+        bookOne.setYearPublication(1985);
         System.out.println(bookOne.getNameBook() + "  " + bookOne.getYearPublication() + " " + authorOne.getNameAuthor());
+        System.out.println(new Author("Roppd", "sefe"));
+        System.out.println(new Book("Dunno on the house", authorOne, 1958));
+
+        System.out.println(bookOne.getYearPublication() + "   " + bookTwo.getYearPublication());
 
     }
 }
